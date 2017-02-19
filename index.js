5fc16dc70e4dda938c6ac8d5d490d5e72958cfbd
Ryan Gaus <rsg1egoman@gmail.com>
2017-02-19 13:20:39 -0500

    code: making code more modular
    - Make a browserifyDecoder, which means we can have more decoders in the
    future
    - Refactor module "backend" to support lookup table-less modules.

--- index.js
+++ index.js
@@ -16,46 +16,76 @@ if (iifeModules.type !== 'ObjectExpression') {
   throw new Error(`The root level IIFE didn't have an object for it's first parameter, aborting...`);
 }
 
+
+
+
+// {
+//   1: [
+//     function(require, module, exports) {
+//       // the code goes here
+//       var foo = require('./foo');
+//     }, {
+//       './foo': 2
+//     }
+//   ],
+//   2: [
+//     function(require, module, exports) {
+//       // this the contents of foo.js
+//     }, {
+//       // No modules to look up
+//     }
+//   ],
+// }
+function browserifyDecoder(moduleArrayAST) {
+  // Loop through each module
+  return moduleArrayAST.properties.map(moduleDescriptor => {
+    // `moduleDescriptor` is the AST for the number-to-array mapping that browserify uses to lookup
+    // modules.
+    if (moduleDescriptor.type !== 'Property') {
+      throw new Error(`The module array AST doesn't contain a Property, make sure that the first argument passed to the rool level IIFE is an object.`);
+    }
+
+    // Extract the identifier used by the module within the bundle
+    let id = moduleDescriptor.key.value;
+    console.log(`* Discovered module ${id}`);
+
+    if (moduleDescriptor.value.type !== 'ArrayExpression') {
+      throw new Error(`Module ${id} has a valid key, but maps to something that isn't an array.`);
+    }
+
+    // Extract the function that wraps the module.
+    let moduleFunction = moduleDescriptor.value.elements[0];
+    console.log(`* Extracted module code for ${id}`);
+
+    // Extract the lookup table for mapping module identifier to its name.
+    let moduleLookup = moduleDescriptor.value.elements[1];
+    if (moduleLookup.type !== 'ObjectExpression') {
+      throw new Error(`Moduel ${id} has a valid key and code, but the 2nd argument passed to the module (what is assumed to be the lookup table) isn't an object.`);
+    }
+    console.log(`* Extracted module lookup table for ${id}`);
+
+    // Using the ast, create the lookup table. This maps module name to identitfier.
+    // To be clear, this just converts the 2nd array arg's AST to a native javascript object.
+    let lookupTable = moduleLookup.properties.reduce((acc, i) => {
+      acc[i.key.value] = i.value.value;
+      return acc;
+    }, {});
+    console.log(`* Calculated module lookup table for ${id}`);
+
+    return {
+      id,
+      code: moduleFunction.body,
+      lookup: lookupTable,
+    };
+  });
+}
+
 // Loop through each module
-let modules = iifeModules.properties.map(moduleDescriptor => {
-  // `moduleDescriptor` is the AST for the number-to-array mapping that browserify uses to lookup
-  // modules.
-  if (moduleDescriptor.type !== 'Property') {
-    throw new Error(`The module array AST doesn't contain a Property, make sure that the first argument passed to the rool level IIFE is an object.`);
-  }
+let modules = browserifyDecoder(iifeModules);
 
-  // Extract the identifier used by the module within the bundle
-  let id = moduleDescriptor.key.value;
-  console.log(`* Discovered module ${id}`);
 
-  if (moduleDescriptor.value.type !== 'ArrayExpression') {
-    throw new Error(`Module ${id} has a valid key, but maps to something that isn't an array.`);
-  }
 
-  // Extract the function that wraps the module.
-  let moduleFunction = moduleDescriptor.value.elements[0];
-  console.log(`* Extracted module code for ${id}`);
 
-  // Extract the lookup table for mapping module identifier to its name.
-  let moduleLookup = moduleDescriptor.value.elements[1];
-  if (moduleLookup.type !== 'ObjectExpression') {
-    throw new Error(`Moduel ${id} has a valid key and code, but the 2nd argument passed to the module (what is assumed to be the lookup table) isn't an object.`);
-  }
-  console.log(`* Extracted module lookup table for ${id}`);
-
-  // Using the ast, create the lookup table. This maps module name to identitfier.
-  let lookupTable = moduleLookup.properties.reduce((acc, i) => {
-    acc[i.key.value] = i.value.value;
-    return acc;
-  }, {});
-  console.log(`* Calculated module lookup table for ${id}`);
-
-  return {
-    id,
-    code: moduleFunction,
-    lookup: lookupTable,
-  };
-});
 
 // Return the values of an object as an array.
 function objectValues(obj) {
@@ -85,11 +115,27 @@ function getModulePath(modules, moduleId) {
   }).find(i => i); // Find the first module that matches.
 }
 
+
+
+
+
+
 // Assemble the file structure on disk.
 modules.map(i => {
-  // Given a module, determine where it was imported within.
-  let moduleHierarchy = getModulePath(modules, i.id);
+  let moduleHierarchy;
   let modulePath;
+
+  // If the module response contains a lookup table for modules that are required in by the current
+  // module being iterated over, then calculate the hierachy of the requires to reconstruct the
+  // tree.
+  if (i.lookup) {
+    // Given a module, determine where it was imported within.
+    console.log(`* Reconstructing require path for module ${i.id}...`);
+    moduleHierarchy = getModulePath(modules, i.id);
+  } else {
+    moduleHierarchy = [`./${i.id}`];
+    console.log(`* No lookup tabie for module ${i.id}, so using identifier as require path...`);
+  }
 
   if (moduleHierarchy === undefined) {
     // Our entry point
@@ -131,14 +177,6 @@ modules.map(i => {
       modulePath = `node_modules/${rootNodeModule}/${modulePath}`;
     }
 
-    // // Determine the name of the file that the cost is in.
-    // // If the path ends with a folder, or is just a node_modules depedency, then the filename should
-    // // be index.js.
-    // let basename = path.basename(moduleHierarchy.slice(-1)[0]);
-    // if (basename === currentNodeModule) {
-    //   basename = "index";
-    // }
-
     console.log(`* ${i.id} => ${modulePath}.js`);
   }
 
@@ -147,9 +185,3 @@ modules.map(i => {
     fs.writeFileSync(`${filePath}.js`, escodegen.generate(i.code));
   });
 });
-
-/* Browserify bundles start with an IIFE.
- *
- */
-
-// console.log(modules);
